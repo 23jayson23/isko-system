f2c08184bf20b14180f7de2772b1a229796267a7
23jayson23 <reyes.jayson5282@gmail.com>
2023-11-17 19:20:01 +0800

    studentadmin

---
+++ adminDashboard/admin.js
@@ -0,0 +1,10 @@
+AOS.init();
+function viewInfo(){
+  var modal_show = document.getElementById("personalinfo")
+  modal_show.style.display = "flex"
+}
+function closeModal(){
+  var modal_show = document.getElementById("personalinfo")
+  modal_show.style.display = "none"
+  console.log(modal_show)
+}
